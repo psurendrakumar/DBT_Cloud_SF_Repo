89b408d9411665e1a4e5482532b7e84bdc86fd36
Pechetti Surendra Kumar <pechettisurendrakumar@gmail.com>
2025-12-30 06:43:20 +0000

    Added gold layer models

---
+++ models/gold_daily_avg_rating.sql
@@ -0,0 +1,11 @@
+select 
+    to_date(r.created_at) as review_date,
+    r.product_id,
+    p.product_name,
+    avg(r.rating) as avg_rating
+from 
+{{ ref('bronze_reviews') }} r 
+left join 
+{{ ref('silver_products') }} p 
+on r.product_id = p.id 
+group by all
