16ea9b8b35f1a0f1c104d79b981c0f6116deaae7
Pechetti Surendra Kumar <pechettisurendrakumar@gmail.com>
2026-01-02 13:30:40 +0000

    Added macros to the project.

--- models/silver/silver_orders.sql
+++ models/silver/silver_orders.sql
@@ -5,6 +5,6 @@ select
     PRODUCT_ID,
     QUANTITY,
     UNIT_PRICE,
-    QUANTITY * UNIT_PRICE as order_amount
+    {{ multiply_columns_and_round('QUANTITY', 'UNIT_PRICE') }} as order_amount
 from 
 {{ ref('bronze_orders') }}
