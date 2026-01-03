84b26d51c16b5973792c9785cc9c3623bf418462
Pechetti Surendra Kumar <pechettisurendrakumar@gmail.com>
2026-01-03 13:05:09 +0000

    Added incremental materialization in bronze_orders model.

--- models/bronze/bronze_orders.sql
+++ models/bronze/bronze_orders.sql
@@ -1,4 +1,15 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key= 'id'
+    )
+}}
+
 select 
 *
 from 
 {{ source('landing','orders') }}
+
+{% if is_incremental %}
+    where created_at > ( select COALESCE(max(created_at),'1900-01-01') from {{this}} )
+{% endif %}
